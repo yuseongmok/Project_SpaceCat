9a98ff06579a9a097689575644f10bdd508cc11c
yuseongmok <202313006@g.shingu.ac.kr>
2023-05-26 10:32:24 +0900

    Space Cat

---
+++ Cat/Assets/02.Scripts/DoT.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoT : MonoBehaviour
+{
+    public bool isPunch = false;
+
+    Sequence sequence;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMoveX(-3000, 20);                            //물체가 X방향으로 5마큼 2초 동안 가겠다.
+
+        sequence = DOTween.Sequence();                  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

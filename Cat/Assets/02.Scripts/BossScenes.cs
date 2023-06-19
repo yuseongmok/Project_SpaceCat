9a6c9c20e5c029cab9c8b2f6a9962c8ecd7a8cdc
yuseongmok <202313006@g.shingu.ac.kr>
2023-06-19 13:55:26 +0900

    몬스터 패턴, 보스전 씬 전환

---
+++ Cat/Assets/02.Scripts/BossScenes.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossScens : MonoBehaviour
+{
+    public string nextScene; // 전환할 다음 씬의 이름
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) // 플레이어 오브젝트와 충돌하면
+        {
+            SceneManager.LoadScene(nextScene); // 다음 씬으로 전환
+        }
+    }
+}
--- Cat/Assets/02.Scripts/Monster/MonsterJump.cs
+++ Cat/Assets/02.Scripts/Monster/MonsterJump.cs
@@ -1,17 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MonsterJump : MonoBehaviour
 {
-    private float timer = 0f;
-
-    public GameObject Player;
+    private bool playerApproaching = false;
+    private bool isJumping = false;
+    private float jumpForce = 100f;
+    private Rigidbody2D rb;
     public float distance;
+    public GameObject Player;
 
-    public void Start()
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Player = GameObject.FindWithTag("Player");
+        isJumping = false;
+    }
+
+    private void Update()
+    {
+        {
+
+            // 플레이어와 몬스터 사이의 거리를 계산
+            distance = Vector2.Distance(transform.position, Player.transform.position);
+
+            // 플레이어가 공격 범위 내에 있으면 점프
+            if (distance <= 110.0f)
+            {
+                if (isJumping == false)
+                {
+                    Jump();
+                }
+            }
+
+
+        }
+
+    }
+
+    private void Jump()
+    {
+        isJumping = true;
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        Invoke(nameof(ResetJump), 1.5f);
+    }
+
+    private void ResetJump()
+    {
+        isJumping = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerApproaching = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerApproaching = false;
+        }
     }
 }
